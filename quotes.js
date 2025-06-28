8ec178538041b43460cfa61e821333e8d54b5db9
higuhouribo <liljee084@gmail.com>
2025-06-28 15:04:16 +0200

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,7 @@
+// For future dynamic quotes or rotating messages
+// Currently not used but left for creative expansion
+const quotes = [
+  "“Take flight with confidence. Predict, Win, and Repeat.”",
+  "“We don't predict the future, we help you play smarter.”",
+  "“Gamble smart. Gamble safe.”"
+];
